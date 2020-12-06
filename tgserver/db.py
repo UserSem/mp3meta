7b71723c371904a434cba9e7994351a26b34b895
Semen Semenov <usersem70@gmail.com>
2020-12-06 15:16:42 +0400

    Add simple telegram bot and db manager

---
+++ tgserver/db.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+from telegram.user import User
+
+import config
+
+
+conn = sqlite3.connect(config.DB_PATH, check_same_thread=False)
+
+def init_db():
+    cursor = conn.cursor()
+    cursor.executescript(
+        "CREATE TABLE IF NOT EXISTS users ("
+        "  id TEXT PRIMARY KEY,"
+        "  name TEXT NOT NULL,"
+        "  is_staff BOOLEAN DEFAULT false"        
+        ");"
+        "CREATE TABLE IF NOT EXISTS membership ("
+        "  group_name TEXT NOT NULL,"
+        "  user_id TEXT NOT NULL,"
+        "  is_admin BOOLEAN default false"
+        ");"
+        "CREATE TABLE IF NOT EXISTS files ("
+        "  group_name TEXT NOT NULL,"
+        "  file_name TEXT NOT NULL"
+        ");"
+        "CREATE TABLE IF NOT EXISTS tags ("
+        "  file_name TEXT NOT NULL,"
+        "  tag_name TEXT NOT NULL,"
+        "  tag_value TEXT"
+        ");"
+        "CREATE TABLE IF NOT EXISTS requests ("
+        "  group_name TEXT NOT NULL,"
+        "  user_id TEXT NOT NULL"
+        ");"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id=?", (config.ADMIN_ID, ))
+    data = cursor.fetchone()
+    if data is None:
+        conn.cursor().execute("INSERT INTO users VALUES (?, ?, ?)",
+                              (config.ADMIN_ID, "Superadmin", True))
+        conn.commit()
+
+
+def add_user(user: User):
+    cursor = conn.execute("SELECT * FROM users where id=?", (user.id, ))
+    data = cursor.fetchone()
+    if data is not None:
+        return None
+    name = [txt for txt in (user.first_name, user.username, user.last_name) if txt is not None]
+    if not name:
+        return "Bad name"
+    conn.execute("INSERT INTO users VALUES (?, ?, ?)", (user.id, ' '.join(name), False))
+    conn.commit()
+
+
+def add_request(group: str, user_id: str):
+    conn.execute("INSERT INTO requests VALUES (?, ?)", (group, user_id))
+    conn.commit()
+    data = conn.execute("SELECT user_id FROM membership WHERE is_admin=true and group_name=?", (group, )).fetchall()
+    if not data:
+        data = conn.execute("SELECT id FROM users WHERE is_staff=true").fetchall()
+    return [d[0] for d in data]
+
+
+init_db()
---
+++ tgserver/run.py
@@ -0,0 +1,44 @@
+from telegram.update import Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from pprint import pprint
+
+import config
+import db
+
+updater = Updater(token=config.TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def addme(group: str, update: Update, context: CallbackContext):
+    err = db.add_user(update.effective_user)
+    if err:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Error adding user ({err})")
+        return
+    admins = db.add_request(group, update.effective_user.id)
+    for admin_id in admins:
+        context.bot.send_message(
+            chat_id=admin_id,
+            text=f"User [{update.effective_user.id}] "
+                 f"{update.effective_user.first_name} "
+                 f"{update.effective_user.username} {update.effective_user.last_name} "
+                 f"requested access to {group}!"
+        )
+
+
+def process(update: Update, context: CallbackContext):
+    try:
+        command = update.message.text.split()
+        if command[0] == 'addme':
+            if len(command) == 2:
+                addme(command[1], update, context)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Command: addme <group>")
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Unknown message!")
+    except Exception as exc:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Unknown error ({exc})")
+
+
+echo_handler = MessageHandler(Filters.all, callback=process)
+dispatcher.add_handler(echo_handler)
+updater.start_polling()
